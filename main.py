679f06d5abc5f9bde58021e9f68348405af17522
David Dobryakov <32088636+kantegory@users.noreply.github.com>
2019-10-08 20:03:14 +0300

    Update main.py
    clean imports, optimize some funcs

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
 import bottle
 from bottle import auth_basic, request, route, template
-from utils.reports import create_report_by_date, add_new_data
+from utils.reports import create_report_by_date, add_new_data, get_date_from_request
 import json
-import datetime
 
 app = bottle.app()
 
 
 @route('/', method='GET')
 def show_main():
-    if request.params:
-        date = request.params.date.split('-')
-        date = '{}.{}.{}'.format(date[2], date[1], date[0])
-    else:
-        date = datetime.datetime.now().strftime('%d.%m.%Y')
+    date = get_date_from_request(request.params)
     rows = create_report_by_date(date)
-    print('this date data', rows)
-    print(type(rows))
     return template('assets/index', date=date, rows=rows, json_data=rows, response_date=request.params.date)
 
 
 @route('/new_report', method='GET')
 def show_report():
     json_data = request.params.data
-    print(json_data)
     add_new_data(json.loads(json_data))
     return json.loads(json_data)
 
@@ -35,17 +27,13 @@ def show_map():
 
 @route('/get_points', method='GET')
 def get_points():
-    if request.params:
-        date = request.params.date.split('-')
-        date = '{}.{}.{}'.format(date[2], date[1], date[0])
-    else:
-        date = datetime.datetime.now().strftime('%d.%m.%Y')
+    date = get_date_from_request(request.params)
     rows = create_report_by_date(date)
     return rows
 
 
 def main():
-    bottle.run(app=app)
+    bottle.run(app=app, port=8081)
 
 
 if __name__ == '__main__':
