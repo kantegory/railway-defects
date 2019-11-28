b286eaf5ca143ae907016fd9299fdb70221a5027
David Dobryakov <32088636+kantegory@users.noreply.github.com>
2019-11-28 13:58:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -62,4 +62,9 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    if os.environ.get('APP_LOCATION') == 'heroku':
+        run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    else:
+        run(host='localhost', port=8080, debug=True)
+
+#     main()
